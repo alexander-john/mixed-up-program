01b4a345fd3304f624ed545a3e80ce79bc4d66ca
Alexander John <alexjohnpersonals@gmail.com>
2022-07-03 08:22:22 -0400

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/checkpoint_2_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/checkpoint_2_1.dir/main.cpp.o"
+  "checkpoint_2_1"
+  "checkpoint_2_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/checkpoint_2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
